170ce16f2f54a7450685160711e10f013b70a5a2
The947thTokay <the947thtokay@codingtime.nl>
2019-09-12 18:47:45 +0200

    Add a really descriptive comment

--- club.red-chair.cylinderaddons/Library/Cylinder/Akribes/Shake.lua
+++ club.red-chair.cylinderaddons/Library/Cylinder/Akribes/Shake.lua
@@ -1,10 +1,9 @@
+-- Effect that swaps icons while making them move left and right.
 return function(page, offset, screen_width, screen_height)
     local percent = offset/page.width
 
     for i, icon in subviews(page) do
         icon:translate(math.sin(2 * math.pi * percent) * 35, 0, 0)
-        --icon.alpha = math.min(math.max(1 - math.pow(percent, 2), 0), 1)
-        --icon.alpha = 1 - math.abs(percent)
         if (percent >= 0) then
             icon.alpha = math.min(1, math.max(0, -8 * percent + 4.5))
         else
